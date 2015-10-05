784c8f9f4917d14bfa0e7bf9d54e2e36a5755c82
Maximo Mena <maximo.mena@meltmedia.com>
2015-10-05 15:39:55 -0700

    Testing encoded single quote.

--- js/app.js
+++ js/app.js
@@ -3,6 +3,13 @@
     console.log("JS");
 
     var title = $('meta[property="og:title"]').attr('content');
+    var url = $('meta[property="og:url"]').attr('content');
     console.log(title);
+
+    var a2a_config = a2a_config || {};
+
+    a2a_config.templates = {
+        twitter: "Reading: ${title} ${link} by @AddToAny"
+    };
 
 })(jQuery);
